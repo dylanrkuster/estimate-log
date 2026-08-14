555f3ce9fb89c6e52bf5a8fe1f603c9dd345fe10
Dylan Kuster <dylanrkuster@gmail.com>
2026-08-13 22:14:43 -0400

    fix(prisma): missing client in production

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,14 @@
-import "dotenv/config";
-
 import { PrismaClient } from "@/prisma/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { env } from "prisma/config";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const adapter = new PrismaPg({
-  connectionString: env("DATABASE_URL"),
-});
-
-export const db = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    }),
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
